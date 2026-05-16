352a3b9bca6534e7df2f0392de2f714753925dc5
k1fir4ig <maxim140906@gmail.com>
2026-05-16 03:10:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
+#include "tstack.h"
+
+std::string infx2pstfx(const std::string& inf);
+int eval(const std::string& pref);
 
 int main() {
-    return 0;
+  std::string infixExpr = "(2+2)*(3-1)";
+  std::string postfixExpr = infx2pstfx(infixExpr);
+  int calcResult = eval(postfixExpr);
+
+  std::cout << "Infix: " << infixExpr << std::endl;
+  std::cout << "Postfix: " << postfixExpr << std::endl;
+  std::cout << "Result: " << calcResult << std::endl;
+
+  return 0;
 }
